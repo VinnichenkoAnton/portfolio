38eb22b68e643201383379a3aa96b226de0dc4be
Anton V <antonnugabest@gmail.com>
2020-12-03 22:42:31 +0200

    add education part without alignment

--- dist/js/script.js
+++ dist/js/script.js
@@ -6,3 +6,10 @@ hamburger.addEventListener('click', () => {
     menu.classList.toggle('menu_active');
     hamburger.classList.toggle("main__hamburger_active");
 });
+
+document.querySelectorAll('.menu__item, .menu__overlay, .menu__link').forEach(function(e) {
+    e.addEventListener('click', function() {
+        menu.classList.remove('menu_active');
+        hamburger.classList.remove('main__hamburger_active');
+    });
+});
