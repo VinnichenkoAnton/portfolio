f7682a2c3339127adc22c7e91b0390e0595cf7a6
Anton V <antonnugabest@gmail.com>
2020-12-17 22:48:45 +0200

    checked mailer, improved min-height, improved hamburger, started adaptation

--- src/js/script.js
+++ src/js/script.js
@@ -65,3 +65,17 @@ $('.contact__form').validate({
         }
     }
 });
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("#inputname, #inputmail, #inputtext").val("");
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
