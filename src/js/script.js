b004d5bcbba3ca63b5b100a0c0ff9fbc82237564
Anton V <antonnugabest@gmail.com>
2020-12-17 12:58:31 +0200

    add all section +validation of the name and mail

--- src/js/script.js
+++ src/js/script.js
@@ -2,11 +2,20 @@ const hamburger = document.querySelector('.main__hamburger'),
     menu = document.querySelector('.menu'),
     closeElem = document.querySelector('.main__hamburger'),
     counters = document.querySelectorAll('.skills__percent'),
-    lines = document.querySelectorAll('.skills__line span');
+    lines = document.querySelectorAll('.skills__line span'),
+    certificates = document.querySelectorAll('.certificates__item');
+
+window.onload = function() {
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function() {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+};
 
 hamburger.addEventListener('click', () => {
     menu.classList.toggle('menu_active');
-    hamburger.classList.toggle("main__hamburger_active");
+    hamburger.classList.toggle('main__hamburger_active');
 });
 
 document.querySelectorAll('.menu__item, .menu__overlay, .menu__link').forEach(function(e) {
@@ -18,4 +27,41 @@ document.querySelectorAll('.menu__item, .menu__overlay, .menu__link').forEach(fu
 
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
+});
+
+certificates.forEach(function(e) {
+    e.addEventListener('mouseover', function() {
+        certificates[2].classList.remove('certificates__item_active');
+        e.classList.add('certificates__item_active');
+    });
+
+    e.addEventListener('mouseout', function() {
+        e.classList.remove('certificates__item_active');
+        certificates[2].classList.add('certificates__item_active');
+    });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 940) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+
+});
+
+$("a[href^='#']").click(function() {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
+
+$('.contact__form').validate({
+    rules: {
+        name: "required",
+        email: {
+            required: true,
+            email: true
+        }
+    }
 });
